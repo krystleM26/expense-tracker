e7afdd4eeea4f2fd15e480bcd768e5ef4f465457
Krystle <krystle@Krystles-MBP.lan>
2024-07-31 19:03:38 -0700

    Setup email and schedule

---
+++ backgroundServices/helpers/sendMail.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer")
+const dotenv = require("dotenv");
+const { create } = require("domain");
+
+
+dotenv.config()
+function createTransporter(config) {
+    const transporter = nodemailer.createTransport(config);
+    return transporter;
+}
+
+let configurations = {
+    service:"gmail",
+    host:"smtp.gmail.com",
+    port:587,
+    requireTLS:true,
+    auth:{
+        user:process.env.EMAIL,
+        password: process.env.PASSWORD
+    }
+}
+
+const sendMail= async(messageOption) => {
+const transporter = await createTransporter(configurations)
+await transporter.verify();
+await transporter.sendMail(messageOption, (error, info) => {
+    if(error){
+        console.log(error);
+    }
+    console.log(info.response);
+})
+}
+
+module.exports=sendMail;
