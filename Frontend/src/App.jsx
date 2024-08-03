96a8f95fb441814d41393f291a2beafbba52f825
Krystle <krystle@Krystles-MBP.lan>
2024-08-03 16:57:39 -0700

    Adding client side

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+     <div className="flex flex-col justify-center items-center mt-[3%] w-[80%] mr-[5%] ml-[5%]">
+      <h1 className="text-2xl font-medium text-[#444]">Expense Tracker</h1>
+      <div className="flex items-center justify-between mt-5 w-[100%]">
+        <div className="relative flex items-center justify-between w-[300px]">
+
+          <button className="bg-[green]">Add Expense</button>
+          <button>Expense Report</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </div>
+    </div>
   )
 }
 
