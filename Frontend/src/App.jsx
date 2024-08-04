e32271bdaf92830d5cca82ce0e1f734ef3b9ab75
Krystle <krystle@Krystles-MBP.lan>
2024-08-03 17:54:43 -0700

    buttons and search created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,8 +10,14 @@ function App() {
       <div className="flex items-center justify-between mt-5 w-[100%]">
         <div className="relative flex items-center justify-between w-[300px]">
 
-          <button className="bg-[green]">Add Expense</button>
-          <button>Expense Report</button>
+          <button className="bg-[grey] p-[10px] border-none outline-none cursor-pointer text-[#fff] text-medium">Add Expense</button>
+          <button className="bg-blue-400 p-[10px] border-none outline-none cursor-pointer text-[#fff] text-medium">Expense Report</button>
+        </div>
+        <div>
+          <input type="text"
+          placeholder="Search"
+          className="p-[10px] w-[150px] border-2 border-[grey] border-solid" 
+          />
         </div>
       </div>
      </div>
