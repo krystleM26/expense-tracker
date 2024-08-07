8bf8f7bd4445e595d58b3204a55c00133f09a5dd
Krystle <krystle@Krystles-MBP.lan>
2024-08-06 22:01:16 -0700

    Client side is complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
 import { FaTrash, FaEdit, FaWindowClose } from "react-icons/fa";
+import { PieChart } from '@mui/x-charts/PieChart';
+
+
 
 function App() {
+
+  const [showAddExpense, setShowAddExpense] = useState(false)
+  
+  const [showReport, setShowReport] = useState(false)
+  const [showEdit, setEdit] = useState(false)
+
+  const handleAddExpense = () => {
+    setShowAddExpense(!showAddExpense)
+  }
+
+  const handleShowReport = () => {
+    setShowReport(!showReport)
+  }
+
+  const handleShowEdit = () => {
+    setEdit(!showEdit)
+  }
+
  
 
   return (
@@ -10,11 +32,13 @@ function App() {
       <div className=" relative flex items-center justify-between mt-5 w-[100%]">
         <div className="relative flex items-center justify-between w-[300px]">
 
-          <button className="bg-[grey] p-[10px] border-none outline-none cursor-pointer text-[#fff] text-medium">Add Expense</button>
-          <button className="bg-blue-400 p-[10px] border-none outline-none cursor-pointer text-[#fff] text-medium">Expense Report</button>
+          <button className="bg-[grey] p-[10px] border-none outline-none cursor-pointer text-[#fff] text-medium" onClick={handleAddExpense}>Add Expense</button>
+          <button className="bg-blue-400 p-[10px] border-none outline-none cursor-pointer text-[#fff] text-medium" onClick={handleShowReport}>Expense Report</button>
         </div>
-        <div className="absolute z-[999] flex flex-col p-[10px] top-[20px] left-0 h-[500px] w-[500px] bg-white shadow-x;">
-          <FaWindowClose className="flex justify-end items-end text-2xl text-red-500 cursor-pointer"/>
+        
+        {showAddExpense && (
+          <div className="absolute z-[999] flex flex-col p-[10px] top-[20px] left-0 h-[500px] w-[500px] bg-white shadow-x;">
+          <FaWindowClose className="flex justify-end items-end text-2xl text-red-500 cursor-pointer" onClick={(handleAddExpense)}/>
           <label htmlFor=""className="mt-[10px] font-semibold text-[18px]">Expense Name</label>
           <input type="text" placeholder="Snacks" className="outline-none border-2 border-[#555] border-solid p-[10px] " />
           <label htmlFor=""  className="mt-[10px] font-semibold text-[18px]">Expense Date</label>
@@ -22,16 +46,34 @@ function App() {
           <label htmlFor="" className="mt-[10px] font-semibold text-[18px]">Expense Amount</label>
           <input type="Number"placeholder="250" className="outline-none border-2 border-[#555] border-solid p-[10px] " />
 
-          <button className="bg-[#af8978] text-white p-[10px] border-none cursor-pointer my-[10px]">Add Expense</button>
+          <button className="bg-[#af8978] text-white p-[10px] border-none cursor-pointer my-[10px] yexy-medium">Add Expense</button>
         </div>
+        )}
 
+        {showReport && 
+          <div className="absolute z-[999] flex flex-col p-[10px] top-[20px] left-[100px] h-[500px] w-[500px] bg-white shadow-x;">
+            <FaWindowClose className="flex justify-end items-end text-2xl text-red-500 cursor-pointer" onClick={handleShowReport}/> 
 
-        <div>
-          <input type="text"
-          placeholder="Search"
-          className="p-[10px] w-[150px] border-2 border-[grey] border-solid" 
-          />
-        </div>
+            <PieChart
+              series={[
+                {
+                  data: [{ id: 0, value: 10, label: 'series A' },
+                  { id: 1, value: 15, label: 'series B' },
+                  { id: 2, value: 20, label: 'series C' },],
+                  innerRadius: 30,
+                  outerRadius: 100,
+                  paddingAngle: 5,
+                  cornerRadius: 5,
+                  startAngle: -90,
+                  endAngle: 180,
+                  cx: 150,
+                  cy: 150,
+                }
+                ]}
+              />
+
+         
+          </div>}
 
        
 
@@ -49,7 +91,7 @@ function App() {
 
           <div className="m-[22px]" >
             <FaTrash className="text-red-500 mb-[5px]  cursor-pointer"/>
-            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer"/>
+            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer" onClick={handleShowEdit}/>
           </div>
         </div>
 
@@ -63,7 +105,7 @@ function App() {
 
           <div className="m-[22px]" >
             <FaTrash className="text-red-500 mb-[5px]  cursor-pointer"/>
-            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer"/>
+            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer" onClick={handleShowEdit}/>
           </div>
         </div>
 
@@ -77,7 +119,7 @@ function App() {
 
           <div className="m-[22px]" >
             <FaTrash className="text-red-500 mb-[5px]  cursor-pointer"/>
-            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer"/>
+            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer" onClick={handleShowEdit} />
           </div>
         </div>
 
@@ -91,7 +133,7 @@ function App() {
 
           <div className="m-[22px]" >
             <FaTrash className="text-red-500 mb-[5px]  cursor-pointer"/>
-            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer"/>
+            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer" onClick={handleShowEdit}/>
           </div>
         </div>
 
@@ -105,10 +147,22 @@ function App() {
 
           <div className="m-[22px]" >
             <FaTrash className="text-red-500 mb-[5px]  cursor-pointer"/>
-            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer"/>
+            <FaEdit className="text-[#555] mb-[5px]  cursor-pointer" onClick={handleShowEdit}/>
           </div>
         </div>
       </div>
+
+      {showEdit &&  <div className="absolute z-[999] flex flex-col p-[10px] top-[15%] right-0 h-[500px] w-[500px] bg-white shadow-x;">
+          <FaWindowClose className="flex justify-end items-end text-2xl text-red-500 cursor-pointer" onClick={handleShowEdit}/>
+          <label htmlFor="" className="mt-[10px] font-semibold text-[18px]">Expense Name</label>
+          <input type="text" placeholder="Snacks" className="outline-none border-2 border-[#555] border-solid p-[10px] " />
+          <label htmlFor=""  className="mt-[10px] font-semibold text-[18px]">Expense Date</label>
+          <input type="date" placeholder="01/01/2024" className="outline-none border-2 border-[#555] border-solid p-[10px] " />
+          <label htmlFor="" className="mt-[10px] font-semibold text-[18px]">Expense Amount</label>
+          <input type="Number" placeholder="250" className="outline-none border-2 border-[#555] border-solid p-[10px] " />
+
+          <button className="bg-[#af8978] text-white p-[10px] border-none cursor-pointer my-[10px] text-medium">Update Expense</button>
+        </div>}
      </div>
     </div>
   )
